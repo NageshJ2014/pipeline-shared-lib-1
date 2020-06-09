60c91eed246eeb24f23e387c703341a43d715313
NageshJ2014 <44293167+NageshJ2014@users.noreply.github.com>
2020-06-10 01:54:23 +0530

    Update buildJavascriptApp.groovy

--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -3,14 +3,17 @@ def call(Map config=[:]) {
     node {
         git url: "https://github.com/werne2j/sample-nodejs"
         stage("Install") {
-            sh "npm install"
+            //sh "npm install"
+            echo "Came Into Install Block"
         }
         stage("Test") {
-            sh "npm test"
+            //sh "npm test"
+            echo "Came Into Test Block"
         }
         stage("Deploy") {
             if (config.deploy) {
-                sh "npm publish"
+                //sh "npm publish"
+                echo "Came Into Deploy Block"
             }
         }
         //body()
